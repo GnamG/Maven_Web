b85987837f5d22b2d87f504cdb3d4b6aa5b8c111
zzx <635702657@q.com>
2022-05-05 20:21:24 +0800

    Swagger3

--- 10-Swagger-03-Swagger3/src/main/java/com/fc/congfig/Swagger3Config.java
+++ 10-Swagger-03-Swagger3/src/main/java/com/fc/congfig/Swagger3Config.java
@@ -1,4 +1,4 @@
-package com.fc.config;
+package com.fc.congfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,20 +7,18 @@ import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
-// 开启Swagger自动装置
-@EnableSwagger2WebMvc
-public class SwaggerConfig {
+public class Swagger3Config {
+    // 装配Swagger的Bean对象:Docket
     // 装配Swagger的Bean对象：Docket
     @Bean
     public Docket docket_Meraki(){
@@ -28,11 +26,36 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo_Meraki())
                 .groupName("Meraki")
-                .globalOperationParameters(token())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fc"))
                 .paths(PathSelectors.ant("/user/**"))
-                .build();
+                .build()
+                // 安全环境
+                .securityContexts(securityContexts())
+                .securitySchemes(securitySchemes());
+    }
+    // 全局配置授权信息
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts = new ArrayList<>();
+        securityContexts.add(getSecurityContext());
+        return securityContexts;
+    }
+    // 安全环境设置
+    private SecurityContext getSecurityContext() {
+        List<SecurityReference> references = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "参数的描述：Token");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+
+        scopes[0] = authorizationScope;
+
+        references.add(new SecurityReference("taken",scopes));
+        return SecurityContext.builder().securityReferences(references).build();
+    }
+
+    // 设置授权信息
+    private List<SecurityScheme> securitySchemes() {
+        ApiKey key = new ApiKey("token", "token", "header");
+        return Collections.singletonList(key);
     }
 
     @Bean
@@ -40,29 +63,13 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo_newL())
                 .groupName("NewL")
-                .globalOperationParameters(token())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fc"))
                 .paths(PathSelectors.ant("/user/**"))
                 .build();
     }
 
-    // 设置全局参数
-    private List<Parameter> token() {
-        ParameterBuilder parameterBuilder = new ParameterBuilder();
-        ArrayList<Parameter> parameters = new ArrayList<>();
-        // 参数名
-        parameterBuilder.name("Authorization")
-                .description("jwt令牌")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .defaultValue("issue token")
-                .scalarExample("header.payload.signature")
-                .required(true);
 
-        parameters.add(parameterBuilder.build());
-        return parameters;
-    }
 
     private ApiInfo apiInfo_Meraki() {
         return new ApiInfoBuilder()
